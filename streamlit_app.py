89ecff6f3afdd087467c52fb5d52de41802e63aa
osbornjunior015-sys <osbornjunior015@gmail.com>
2026-04-27 00:53:13 -1200

    Fix CSV URL in Fraud Detection App

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import pandas as pd
 st.title('🚩 Fraud Detection App')
 
 st.info('This App builds a Fraud Detection')
-df = pd.read_csv('https://github.com/osbornjunior015-sys/cleaned-fruad.data/blob/main/cleaned_fraud_data.csv')
+df = pd.read_csv('https://github.com/osbornjunior015-sys/cleaned-fruad.data')
