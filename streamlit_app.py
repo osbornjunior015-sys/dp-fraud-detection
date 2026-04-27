c7df8bf273ac6eb2cdf833750478e1ee52a431da
osbornjunior015-sys <osbornjunior015@gmail.com>
2026-04-27 00:50:12 -1200

    Add pandas import and load CSV data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🚩 Fraud Detection App')
 
 st.info('This App builds a Fraud Detection')
+df = pd.read_csv('https://github.com/osbornjunior015-sys/cleaned-fruad.data/blob/main/cleaned_fraud_data.csv')
