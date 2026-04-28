494703ef157f278685747a7a9702be4a8751cc70
osbornjunior015-sys <osbornjunior015@gmail.com>
2026-04-28 00:48:11 -1200

    Refactor data display code in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,18 +6,18 @@ import  joblib
 st.title('🚩 Fraud Detection Preadiction App')
 st.write("Enter transaction details to check if it's fraudulent.")
 with st.expander('Data'):
-     st.write('**Raw Data**')
-     st.info('This App builds a Fraud Detection')
-     df = pd.read_csv('https://raw.githubusercontent.com/osbornjunior015-sys/cleaned-fruad.data/refs/heads/main/cleaned_fraud_data.csv')
-     df
+  st.write('**Raw Data**')
+  st.info('This App builds a Fraud Detection')
+  df = pd.read_csv('https://raw.githubusercontent.com/osbornjunior015-sys/cleaned-fruad.data/refs/heads/main/cleaned_fraud_data.csv')
+  df
 
-     st.write('**X**')
-     x = st.drop('step', axis=1)
-     x
+  st.write('**X**')
+  x = st.drop('step', axis=1)
+  x
 
-     st.write('**y**')
-     y = df.step
-     y
+  st.write('**y**')
+  y = df.step
+  y
 
 
 
