f8b406137444ddcb51df8ece4397b9674851d455
osbornjunior015-sys <osbornjunior015@gmail.com>
2026-04-28 00:11:26 -1200

    Add info message for Fraud Detection App
    Added information message about the app's purpose.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ model = joblib.load("fraud_model.pkl")
 
 st.title('🚩 Fraud Detection Preadiction App')
 st.write("Enter transaction details to check if it's fraudulent.")
+
 st.info('This App builds a Fraud Detection')
 df = pd.read_csv('https://raw.githubusercontent.com/osbornjunior015-sys/cleaned-fruad.data/refs/heads/main/cleaned_fraud_data.csv')
 df
