9e7ed9905c94662f96162d0869854cec6671e851
osbornjunior015-sys <osbornjunior015@gmail.com>
2026-04-28 00:09:50 -1200

    Update title and improve user instructions

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import  joblib
 
-st.title('🚩 Fraud Detection Application')
+model = joblib.load("fraud_model.pkl")
 
-with st.expander('Data'):
-   st.write('**Raw Data**')
-   st.info('This App builds a Fraud Detection')
-   df = pd.read_csv('https://raw.githubusercontent.com/osbornjunior015-sys/cleaned-fruad.data/refs/heads/main/cleaned_fraud_data.csv')
-   df
+st.title('🚩 Fraud Detection Preadiction App')
+st.write("Enter transaction details to check if it's fraudulent.")
+ st.info('This App builds a Fraud Detection')
+ df = pd.read_csv('https://raw.githubusercontent.com/osbornjunior015-sys/cleaned-fruad.data/refs/heads/main/cleaned_fraud_data.csv')
+ df
